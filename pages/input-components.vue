1e0f600ff287603be397686f3b99021b52a483ed
01Maku <136875476+01Maku@users.noreply.github.com>
2023-10-02 23:02:40 +0800

    added vue component

---
+++ pages/input-components.vue
@@ -0,0 +1,80 @@
+<template lang="en">
+    <label>{{ objectTemplate.label }}</label>
+    <input required v-model="provNullModel[cloneObjectTemplate.model]" :type="objectTemplate.type" @change="emitter(cloneObjectTemplate.model, $event.target.value, dataFlag)">
+</template>
+<script>
+export default
+{
+    inject:
+    [
+        'provNullModel',
+    ],  
+    props:
+    {
+        objectTemplate:
+        {
+            type: Object,
+            required: true,
+        },
+        dataFlag:
+        {
+            type: String,
+            required: true,
+        }
+    },
+
+    computed:
+    {
+        cloneObjectTemplate()
+        {
+            return structuredClone(this.objectTemplate)
+        }
+    },
+    methods:
+    {
+        /**
+         * Handles the emitting of data to parent, emits input data.
+         */
+        emitter(paramModel, paramData, paramFlag)
+        {
+            const objectData =
+            {
+                flag: paramFlag,
+                model: paramModel,
+                data: paramData,
+            }
+            this.$emit('emitData', objectData )
+        }
+    }
+}
+</script>
+<style scoped>
+.parent-margin
+{
+    margin-left: 20%;
+    margin-right: 15%;
+}
+.margin
+{
+    margin: 10px;
+}
+.padding
+{
+    padding: 10px;
+}
+.flex
+{
+    display: flex;
+}
+.column
+{
+    flex-direction: column;
+}
+.std-border
+{
+    border-width: 2px;
+    border-radius: 3px;
+    border-style: solid;
+    border-color: black;
+}
+</style>
