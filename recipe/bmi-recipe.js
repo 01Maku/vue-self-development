aee1be6c439e10a7bbe7bf946795be04d3fb04d1
01マクス <136875476+01Maku@users.noreply.github.com>
2023-10-03 00:06:32 +0800

    Update bmi-recipe.js

--- recipe/bmi-recipe.js
+++ recipe/bmi-recipe.js
@@ -1,8 +1,8 @@
 const inputTemplate =
 [
-    {label: 'Name', type: 'text', model: 'inputName'},
-    {label: 'Height (cm)', type: 'number', model: 'inputHeight'},
-    {label: 'Weight (kg)', type: 'number', model: 'inputWeight'},
+    {step:"0.01", label: 'Name', type: 'text', model: 'inputName'},
+    {step:"0.01", label: 'Height (m)', type: 'number', model: 'inputHeight'},
+    {step:"0.01", label: 'Weight (kg)', type: 'number', model: 'inputWeight'},
 ]
 
 const dataCluster =
